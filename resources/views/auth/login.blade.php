4b7fcc1e936f16682ec85239d6cc78d3c3a96a2c
Alf <alf@drollinger.info>
2021-03-07 02:45:19 +0100

    TallUI V1 running

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,14 @@
 <x-guest-layout>
 
-    <div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div class="flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
 
-        <div class="sm:mx-auto sm:w-full sm:max-w-md flex flex-col items-center">
-            <x-tallui.logo />
+        <div class="flex flex-col items-center sm:mx-auto sm:w-full sm:max-w-md">
+            <x-tallui.svg.logo />
         </div>
 
         <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+
+            <!-- livewire:tallui:login-component / -->
 
             <livewire:login-form />
 
--- resources/views/components/tallui/checkbox.blade.php
+++
@@ -1,1 +0,0 @@
-<input type="checkbox" {!! $attributes->merge(['class' => 'h-4 w-4 text-cyan-600 focus:ring-cyan-500 border-gray-300 rounded focus:ring-opacity-50']) !!}>
